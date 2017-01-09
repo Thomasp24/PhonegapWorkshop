3c1e8af798a040632c7068acf6cf132b13694adc
Thomas Peters <thomaspeters4@outlook.com>
2017-01-09 12:28:21 +0100

    working on camera

--- Phonegap Workshop/www/js/camera.js
+++ Phonegap Workshop/www/js/camera.js
@@ -5,7 +5,17 @@ var cameraInit = function () {
     var camera = navigator.camera;
     $$("#TakePhoto").on("click", function (e) {
         App.alert("bladiebla");
-        camera.getPicture(succesCallback, errorCallback, {});
+        camera.getPicture(succesCallback, errorCallback, {
+            // Some common settings are 20, 50, and 100
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            // In this app, dynamically set the picture source, Camera or photo gallery
+            sourceType: Camera.PictureSourceType.CAMERA,
+            encodingType: Camera.EncodingType.JPEG,
+            mediaType: Camera.MediaType.PICTURE,
+            allowEdit: true,
+            correctOrientation: true  //Corrects Android orientation quirks
+        });
     });
 
     var succesCallback = function (data) {
